b707848cd125bdfb1d7757f73bdd60d717563b6b
unknown <roland8410@hotmail.com>
2022-07-25 15:36:15 -0600

    funcionesjs

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # platzi-curso-practico-javascript1
+
+...
+
+## Taller #1: figuras geometricas
+
+- primer paso: definir las formulas.
+- segundo paso: implementar las formulas en js.
+- tercer paso: crear funciones.
+- cuarto paso: integran js por html.
---
+++ js/funciones.js
@@ -0,0 +1,73 @@
+//codigo para el cuadrado
+console.group("cuadrado");
+// const ladoCuadrado = 5;
+
+// console.log('los lados del cuadrado miden: ' + ladoCuadrado);
+
+function perimetroCuadrado (lado){
+   return lado *4;
+}
+
+// console.log('el permitetro del cuadrado es:'+ perimetro + ' unidades');
+
+function areaCuadrado(lado){
+return lado *lado;
+} 
+// console.log('el area del cuadrado es: '+ area + 'unidades cuadradas');
+
+console.groupEnd();
+
+// codigo para el triangulo
+console.group("triangulo");
+// const ladotrianquilo1 = 6;
+// const ladotrianquilo2 = 6;
+// const base = 4;
+// const altura=5.5;
+// console.log('los lados del triangulo mider: '+ ladotrianquilo1 + ' cm '+ ladotrianquilo2 + ' cm '+ base + ' cm');
+
+
+// console.log("la altura del triangulo es: " +altura);
+
+function periTrian(ladotrianquilo1,ladotrianquilo2,base){
+    return ladotrianquilo1+ladotrianquilo2+base;
+}
+// console.log("el permitro del triangulo es : "+periTrian+" cm");
+
+function areaTrin(base,altura) {
+   return (base*altura)/2;
+}
+    
+// console.log("el area del trangulo es : "+areaTrin+" cm2");
+console.groupEnd();
+
+//codigo  del circulo
+console.group("circulo");
+
+//variable radio
+// const radioCirculo = 4;
+// console.log("el radio del circulo es : "+radioCirculo)
+//variable diametro
+function diametro(radioCirculo){
+   return radioCirculo*2;
+}
+// console.log("el diametro del circulo es : "+diametro)
+
+//variable pi
+const PI= Math.PI;
+// console.log("PI es : "+ PI)
+
+// circunferecnia
+function periCircunferencia(radio){
+    const diametroCirculo = diametro(radio);
+    return  diametroCirculo * PI;
+} 
+// console.log("la circunferencia del circulo es : "+periCircunferencia)
+
+//area
+function areaCir(radio){
+
+   return (radio*radio)*PI;
+} 
+// console.log("el area del circulo es : "+areaCir);
+
+console.groupEnd();
